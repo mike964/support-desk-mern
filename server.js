187a8ddbd9e1b42950476958aad244c6097f5264
Amen Kemal <57805638+mike964@users.noreply.github.com>
2022-05-08 22:23:23 +0300

    local mongodb added. server and client work fine

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const { errorHandler } = require('./server/middleware/errorMiddleware')
+const connectDB = require('./server/config/db')
 const PORT = process.env.PORT || 5000
 
 // Connect to database
@@ -15,22 +15,22 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/tickets', require('./routes/ticketRoutes'))
+app.use('/api/users', require('./server/routes/userRoutes'))
+app.use('/api/tickets', require('./server/routes/ticketRoutes'))
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  // Set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  // FIX: below code fixes app crashing on refresh in deployment
-  app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-  })
+	// FIX: below code fixes app crashing on refresh in deployment
+	app.get('*', (_, res) => {
+		res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+	})
 } else {
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' })
-  })
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome to the Support Desk API' })
+	})
 }
 
 app.use(errorHandler)
